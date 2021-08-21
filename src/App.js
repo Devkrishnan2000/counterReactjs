2e2354f65a49076981f59eb37f2ffb31545ad875
Devkrishnan2000 <devanaswinikumar8@gmail.com>
2021-08-21 14:59:24 +0530

    first program using reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [count, setcount] = useState(0);
+
+  const addcount =()=>
+  {
+    setcount((prev=>prev+1));
+  }
+
+  const remcount =()=>
+  {
+    setcount((prev=>prev-1));
+  }
+  const rst =()=>
+  {
+    setcount(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>COUNTER</h1>
+     <div class="parentdiv">
+      
+      <div class="Circle">  
+          <div class="new_row">
+              <div class="Display">
+                  <h2>{count}</h2>
+              </div> 
+          </div>
+          <div class="new_row">
+              <button class="startbtn" onClick={rst}>RESET</button>
+          </div> 
+           <div class="new_row">
+               <button class="circlebtns leftbtn" onClick={addcount}>+</button>
+               <button class="circlebtns rightbtn"onClick={remcount}>-</button>
+           </div> 
+      </div>
+     
+  </div>
     </div>
   );
 }
